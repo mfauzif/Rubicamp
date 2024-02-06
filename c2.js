534d926645aa495aa4677c8c7e9960b862124d56
mfauzif <febriansyahmuhamadfauzi@gmail.com>
2024-02-06 14:02:32 +0700

    Revisi Ch2

--- c2.js
+++ c2.js
@@ -1,12 +1,12 @@
 function deretKaskus(n) {
     let array = []
-    for (let i = 3; i <= 30; i += 3) {
-        if (i % 5 === 0) {
+    for (let i = 3; i <= (n * 3); i += 3) {
+        if (i % 5 === 0 && i % 6 === 0) {
+            array.push("KASKUS")
+        } else if (i % 5 === 0) {
             array.push("KAS")
         } else if (i % 6 === 0) {
             array.push("KUS")
-        } else if (i % 5 && i % 6 === 0) {
-            array.push("KASKUS")
         } else {
             array.push(i);
         }
