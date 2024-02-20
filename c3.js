6bfb25f3c404f6b3873eee44ca47b2f031ed4dd2
mfauzif <febriansyahmuhamadfauzi@gmail.com>
2024-02-20 14:30:52 +0700

    ch3

---
+++ c3.js
@@ -0,0 +1,38 @@
+const romawi = n => {
+    //write your code here
+    let result = '';
+
+    const numerals = [
+        { value: 1000, numeral: 'M' },
+        { value: 900, numeral: 'CM' },
+        { value: 500, numeral: 'D' },
+        { value: 400, numeral: 'CD' },
+        { value: 100, numeral: 'C' },
+        { value: 90, numeral: 'XC' },
+        { value: 50, numeral: 'L' },
+        { value: 40, numeral: 'XL' },
+        { value: 10, numeral: 'X' },
+        { value: 9, numeral: 'IX' },
+        { value: 5, numeral: 'V' },
+        { value: 4, numeral: 'IV' },
+        { value: 1, numeral: 'I' }
+    ];
+
+    for (let i = 0; i < numerals.length; i++) {
+        while (n >= numerals[i].value) {
+            result += numerals[i].numeral;
+            n -= numerals[i].value;
+        }
+    }
+
+    return result;
+};
+
+console.log("Script Testing untuk Konversi Romawi\n");
+console.log("input | expected | result");
+console.log("------|----------|-------");
+console.log("4     | IV       | ", romawi(4));
+console.log("9     | IX       | ", romawi(9));
+console.log("13    | XIII     | ", romawi(13));
+console.log("1453  | MCDLIII  | ", romawi(1453));
+console.log("1646  | MDCXLVI  | ", romawi(1646));
